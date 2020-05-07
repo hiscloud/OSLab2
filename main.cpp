5c15177b235f6cb795973bff43725ab589d98e6f
hiscloud <49000237+hiscloud@users.noreply.github.com>
2020-05-06 19:15:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include "err.h"
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+
 using namespace std;
 
-// count how many occurance a word(L&U) in a file, input must be a lowercase number
-// int(a)
+// count how many occurance a word(Lower&Upper) in a file, input must be a lowercase number
 // a-32=A a=97
 int count(char wordN, string filename)
 {
@@ -24,8 +27,7 @@ int count(char wordN, string filename)
       if (test==wordN||test==(wordN-32))
         counter++;
       infile>>test;
-  //  cout<<test<<endl;
-          
+       
     }
     infile.close();
     return counter;
@@ -53,34 +55,41 @@ int main(int argc, char* argv[])
       filename+=" ";
       filename+=argv[i];
     }
-
-  int occ[26];
+  //http://www.cplusplus.com/forum/general/245501/
+    key_t key = 0;
+    int shm_id;
+    shm_id = shmget(key, 26*sizeof(int), 0666 | IPC_CREAT);
+    int* occ = (int*)shmat(shm_id, NULL, 0);
+  //
+  //int* occ=new int[26];               
   pid_t pid;  
-    pid=fork();
+  pid=fork();
   for(int i=97;i<97+26;i++)
   {
        
     if(pid==0)
     {
-        char letter=i;
-      cout<<letter<<" ";
-     occ[i-97]=count(i,filename);
-        cout<<occ[i-97]<<endl;
-      exit(-1);
+      occ[i-97]=count(i,filename);
+      exit;
     }else if (pid<0)
     {
       cout<<"fork error\n";
-      exit(-1);
-    }else
-    {
-      break;
       exit;
+    } 
+    
+  }
+  
+  if (pid>0)
+  { 
+  wait(NULL);
+    for (int i=0;i<26;i++)
+    {
+      char Lletter=i+97;
+      char Uletter=Lletter-32;
+      cout<<Uletter<<"/"<<Lletter<<" ";
+      cout<<occ[i]<<endl;
     }
   }
-  if (pid>0){/////
-    for (int i=0;i<26;i++)
-      cout<<occ[i]<<endl;
-  // exit(-1);
-  }
+  
   return 0;
 }
